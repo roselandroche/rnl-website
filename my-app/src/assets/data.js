dab450633e38ac6f082fd90a908faaef61024ffe
Rose Landroche <r.landroche@gmail.com>
2021-12-09 13:26:03 -0800

    content for history

---
+++ my-app/src/assets/data.js
@@ -0,0 +1,87 @@
+// history
+export const jobs = [
+  {
+    company: "Jambaree",
+    title: "Web Developer",
+    timeframe: "Apr 2021 - Nov 2021",
+    desc: [
+      "Brought wireframes to life using Gatsby, React, JavaScript, and Theme UI.",
+      "Built responsive websites for multiple clients.",
+      "Maintained an organized workflow using GitHub, GitLab, Asana, and Slack.",
+    ],
+    references: true,
+  },
+  {
+    company: "GrantMe",
+    title: "Junior Web Developer",
+    timeframe: "Nov 2020 - Apr 2021",
+    desc: [
+      "Meticulously completed QA of new features for web application",
+      "Efficiently maintained and updated WordPress site",
+      "Independently managed and completed custom WordPress build within deadline",
+      "Managed Hubspot CRM for multiple pipelines with custom logic",
+    ],
+    references: true,
+  },
+];
+
+export const edu = [
+  {
+    institution: "Lambda School (now Bloom Institute of Technology)",
+    credentialEarned: "Full Stack Web Development",
+    timeframe: "2019 - 2020",
+  },
+  {
+    institution: "Kingswood University (formerly Bethany Bible College)",
+    credentialEarned: "Bachelor's of Arts in Music Ministry",
+    timeframe: "2010 - 2014",
+  },
+  {
+    institution: "Hilbert College",
+    credentialEarned: "Associate of Arts in Criminal Justice",
+    timeframe: "2008 - 2010",
+  },
+];
+
+// about
+export const tech = [
+  {
+    name: "",
+    useCase: "",
+    profExp: false,
+    persExp: true,
+    relatedProjects: [
+      {
+        name: "",
+        github: "",
+        deployment: "",
+      },
+    ],
+  },
+  {
+    name: "",
+    useCase: "",
+    profExp: false,
+    persExp: true,
+    relatedProjects: [
+      {
+        name: "",
+        github: "",
+        deployment: "",
+      },
+    ],
+  },
+  {
+    name: "",
+    useCase: "",
+    profExp: false,
+    persExp: true,
+    relatedProjects: [
+      {
+        name: "",
+        github: "",
+        deployment: "",
+      },
+    ],
+  },
+];
--- my-app/src/pages/History.js
+++ my-app/src/pages/History.js
@@ -1,11 +1,20 @@
 import React from "react";
+
 import "../css/global.css";
+import "../css/history.css";
+import { jobs } from "../assets/data";
 import Resume from "../assets/Resume-RoseLandroche.pdf";
+import JobCard from "../components/JobCard";
 
 function History() {
   return (
     <div className="main">
       <h1>History</h1>
+      <h2>Most Recent Employment</h2>
+      {jobs &&
+        jobs.map((o) => {
+          return <JobCard job={o} />;
+        })}
       <a href={Resume} download="Resume-RoseLandroche">
         Resume - Updated December 2021
       </a>
