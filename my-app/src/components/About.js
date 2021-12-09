a6b19d9a4c98bc037b941f82f494102deb20fd9a
Rose Landroche <r.landroche@gmail.com>
2021-12-08 16:29:03 -0800

    content, resume file

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -5,6 +5,9 @@ function About() {
   return (
     <div className="main">
       <h1>About</h1>
+      <h2>I love working with this tech:</h2>
+      <h2>I love working with these causes:</h2>
+      <h2>I love spending time on these hobbies:</h2>
     </div>
   );
 }
--- my-app/src/components/History.js
+++ my-app/src/components/History.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../css/global.css";
+import Resume from "../assets/Resume-RoseLandroche.pdf";
 
 function History() {
   return (
     <div className="main">
       <h1>History</h1>
+      <a href={Resume} download="Resume-RoseLandroche">
+        Resume - Updated December 2021
+      </a>
     </div>
   );
 }
