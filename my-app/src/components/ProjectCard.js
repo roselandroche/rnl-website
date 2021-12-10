8f3d33730c61e1aa574de9740d7ccb7c474c2eff
Rose Landroche <r.landroche@gmail.com>
2021-12-10 15:59:20 -0800

    structure

--- my-app/src/components/ProjectCard.js
+++ my-app/src/components/ProjectCard.js
@@ -5,18 +5,39 @@ function ProjectCard(props) {
 
   return (
     <div className="projCard">
-      {proj.company && proj.title && (
-        <h3>
-          {proj.company} | {proj.title}
-        </h3>
-      )}
-      {proj.timeframe && <h4>{proj.timeframe}</h4>}
-      <ul>
-        {proj.desc &&
-          proj.desc.map((item) => {
-            return <li className="projDesc">{item}</li>;
-          })}
-      </ul>
+      <div className="image">
+        {proj.image && <img src={proj.image} alt="Project user interface" />}
+      </div>
+      <div className="text">
+        {proj.title && proj.role && (
+          <h3>
+            {proj.title} | {proj.role}
+          </h3>
+        )}
+        {proj.github && (
+          <div>
+            GitHub: <a href={proj.github}>{proj.github}</a>
+          </div>
+        )}
+        {proj.deploy && (
+          <div>
+            Deployed: <a href={proj.deploy}>{proj.deploy}</a>
+          </div>
+        )}
+        <div className="stack">
+          {proj.tech &&
+            proj.tech.map((item) => {
+              return <h5>{item}</h5>;
+            })}
+        </div>
+        <ul>
+          {proj.desc &&
+            proj.desc.map((item) => {
+              return <li className="projDesc">{item}</li>;
+            })}
+        </ul>
+        {proj.stage && <h6>Stage: {proj.stage}</h6>}
+      </div>
     </div>
   );
 }
