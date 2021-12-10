ec21b666898f6c583172fe2dabe18b148bbe9626
Rose Landroche <r.landroche@gmail.com>
2021-12-10 14:56:13 -0800

    create proj map

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -1,11 +1,17 @@
 import React from "react";
+
 import "../css/global.css";
+import { proj } from "../assets/data";
+import ProjectCard from "../components/ProjectCard";
 
 function Projects() {
   return (
     <div className="main">
       <h1>Projects</h1>
-      <h2>...</h2>
+      {proj &&
+        proj.map((o) => {
+          return <ProjectCard proj={o} />;
+        })}
     </div>
   );
 }
