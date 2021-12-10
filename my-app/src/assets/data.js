e2999cd9a48421ee72568e8aa86adb836405aa3a
Rose Landroche <r.landroche@gmail.com>
2021-12-10 14:55:57 -0800

    proj data

--- my-app/src/assets/data.js
+++ my-app/src/assets/data.js
@@ -43,6 +43,69 @@ export const edu = [
   },
 ];
 
+// projects
+export const proj = [
+  {
+    title: "Professional History",
+    role: "Full Stack Developer",
+    stage: "In Progress",
+    tech: [
+      "Gatsby",
+      "React",
+      "JavaScript",
+      "Typescript",
+      "Cypress",
+      "ThemeUI",
+      "Netlify",
+      "Git",
+    ],
+    desc: [
+      "A safe, convenient, and central place for people to save data they need when searching for a job",
+      "Creatively discovered user need, and designed custom user experience",
+      "Skillfully built clean, reusable modular components",
+    ],
+    github: "https://github.com/roselandroche/professional",
+    deploy: "https://professional-history.netlify.app/",
+    image: "",
+  },
+  {
+    title: "Conway’s Game of Life",
+    role: "Full Stack Developer",
+    stage: "Complete - For Now",
+    tech: ["React", "JavaScript", "CSS", "Netlify", "Git"],
+    desc: [
+      "A SPA visualizing John Conway's Game of Life, including configurations, generation counter, and explanation of basic rules",
+      "Resourcefully utilized resources to research and plan a fully functional application",
+      "Skillfully built the architecture and algorithms of the project",
+    ],
+    github: "https://github.com/roselandroche/game_of_life",
+    deploy: "https://awesome-ramanujan-7d7ef5.netlify.app/",
+    image: "",
+  },
+  {
+    title: "Save This Job",
+    role: "Back End Engineer",
+    stage: "Complete",
+    tech: [
+      "Node",
+      "Express",
+      "Knex",
+      "Supertest",
+      "SQLite",
+      "PostgreSQL",
+      "Heroku",
+    ],
+    desc: [
+      "An application built to give users the ability to save job postings and related information as they search for their next career",
+      "Collaboratively worked with a cross functional team to create a fully functional web application and Chrome Extension",
+      "Coordinated with other team members to create a well tested and seeded back end with numerous endpoints",
+    ],
+    github: "https://github.com/roselandroche/job-book-be",
+    deploy: "https://save-this-job.netlify.app/",
+    image: "",
+  },
+];
+
 // about
 export const tech = [
   {
